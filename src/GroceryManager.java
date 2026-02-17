9ad6604c194a289141e67c230d67a991843245ae
Viraj Ghimire <viraj@Mac-1474.lan>
2026-02-17 01:01:54 -0600

    Add assignment skeleton with Javadoc

---
+++ src/GroceryManager.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ * GroceryManager - A grocery management system using parallel arrays.
+ * Manages item names, prices, and stock levels.
+ *
+ * @author Biraj Ghimire, Alok Parajuli, John Nichols
+ */
+public class GroceryManager {
+
+    /**
+     * Main method - runs the user menu.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+
+        // TODO (John): implement user menu here
+    }
+
+    /**
+     * Prints all items in the inventory.
+     * @param names array of item names
+     * @param prices array of item prices
+     * @param stocks array of item stock levels
+     */
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+        // TODO (Alok): implement this method
+    }
+
+    /**
+     * Restocks an item by name.
+     * @param names array of item names
+     * @param stocks array of item stock levels
+     * @param target the item name to restock
+     * @param amount the amount to add to stock
+     */
+    public static void restockItem(String[] names, int[] stocks, String target, int amount) {
+        // TODO (Biraj): implement this method
+    }
+}
